03d060764f439649795ae734f8f0f8fa3312f534
harmbrugge <harmbrugge@gmail.com>
2014-11-20 13:02:59 +0100

    JSON test module

---
+++ Databases/json_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+import database
+
+
+def main():
+    print("Content-type: application/json")
+    print()
+
+    print(json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]))
+
+
+if __name__ == '__main__':
+    main()
+
+
